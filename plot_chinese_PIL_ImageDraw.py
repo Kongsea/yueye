a692de333f4a6faae53db67885bf38a4a4a9d6fe
Kongsea <Kongsea@gmail.com>
2018-02-10 17:42:38 +0800

    Refine.

--- plot_chinese_PIL_ImageDraw.py
+++ plot_chinese_PIL_ImageDraw.py
@@ -9,16 +9,16 @@ image = Image.open('{}.jpg'.format(image_name))
 
 color = 'red'
 (left, top, right, bottom) = (270, 120, 420, 370)
+class_name = '老虎'
 
-draw = ImageDraw.Draw(image)
-draw.line([(left, top), (left, bottom), (right, bottom),
-           (right, top), (left, top)], width=1, fill=color)
 try:
   font = ImageFont.truetype('wqy-microhei.ttc', 14)
 except IOError:
   font = ImageFont.load_default()
 
-class_name = '老虎'
+draw = ImageDraw.Draw(image)
+draw.line([(left, top), (left, bottom), (right, bottom),
+           (right, top), (left, top)], width=1, fill=color)
 
 draw.text(
     (left, top-14),
@@ -26,4 +26,4 @@ draw.text(
     fill='black',
     font=font)
 
-image.save('test.jpg')
+image.show()
